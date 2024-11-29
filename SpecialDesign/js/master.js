ec5f915c08d43b57ee675d9039905732edc3e98e
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-11-29 13:39:26 +0100

    finsh the box setting  and startin in the bullets section

---
+++ SpecialDesign/js/master.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+// Click On Toggle Settings Gear
+document.querySelector(".toggle-settings .fa-gear").onclick = function () {
+
+  // Toggle Class Fa-spin For Rotation on Self
+  this.classList.toggle("fa-spin");
+
+  // Toggle Class Open On Main Settings Box
+  document.querySelector(".settings-box").classList.toggle("open");
+  
+};
+
+;
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
